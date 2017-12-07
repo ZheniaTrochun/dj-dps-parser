096c51878fe5931e64580e9343c10c1840382eb5
evgenijaZ <evg.zubrich@gmail.com>
2017-12-07 16:54:39 +0200

    added more files for testing

---
+++ test/examples/set.js
@@ -0,0 +1,16 @@
+set('tags')
+
+<?html
+  DB totals:<br/>datasets:&nbsp;
+?>
+// test
+
+/*
+  test
+*/
+
+append({{datasets}})
+wrap(
+  tag:'div', style:'margin:0'
+)
+html()
--- test/parsing.test.js
+++ test/parsing.test.js
@@ -2,21 +2,46 @@ const parser = require('../index');
 const fs = require('fs');
 
 
-function getParsed(path) {
+function getParsed(fileName) {
+    let path = 'examples/' + fileName;
     const data = fs.readFileSync(path);
-    path ='test/examples/'+path;
     const parsed = (new parser().config()).parse(data.toString());
-    return (JSON.stringify(parsed, undefined, 1));
+    return  (JSON.stringify(parsed, undefined, 1));
 
 }
 
-function getSample(path) {
-     path ='test/examples/samples/'+path;
-    return (fs.readFileSync(path)).toString();
+function getSample(fileName) {
+    let path = 'examples/samples/' + fileName;
+    return (fs.readFileSync(path).toString());
 
 }
 
-test('positive test of parsing', () => {
-    fileName='getVbar.js';
- expect(getParsed(fileName)).toBe(getSample(fileName));
+test('positive test of parsing getVbar.js', () => {
+    const fileName = 'getVbar.js';
+    expect(getSample(fileName)).toBe(getParsed(fileName));
+});
+
+test('positive test of parsing getDeps.js', () => {
+    const fileName = 'getDeps.js';
+    expect(getParsed(fileName)).toBe(getSample(fileName));
+});
+
+test('positive test of parsing pca.js', () => {
+    const fileName = 'pca.js';
+    expect(getParsed(fileName)).toBe(getSample(fileName));
+});
+
+test('positive test of parsing feedView.js', () => {
+    const fileName = 'feedView.js';
+    expect(getParsed(fileName)).toBe(getSample(fileName));
+});
+
+test('positive test of parsing rssTimeLine.js', () => {
+    const fileName = 'rssTimeLine.js';
+    expect(getParsed(fileName)).toBe(getSample(fileName));
+});
+
+test('positive test of parsing getTimeSerie.js', () => {
+    const fileName = 'getTimeSerie.js';
+    expect(getParsed(fileName)).toBe(getSample(fileName));
 });
