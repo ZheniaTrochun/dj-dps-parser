103372066190cf65078b0a81d0968e461e5c82a2
evgenijaZ <evg.zubrich@gmail.com>
2017-12-07 22:03:23 +0200

    added test for braces

--- test/bracketsError.test.js
+++ test/bracketsError.test.js
@@ -1,11 +1,19 @@
 const parser = require('../src/parser');
 const fs = require('fs')
 
-test('test invalid number of brackets', () => {
+test('test invalid number of brackets ()', () => {
   function wrapper () {
-    const text = fs.readFileSync('examples/wrong/bracketsErr.js');
+    const text = fs.readFileSync('examples/wrong/pca_brackets.js');
     return new parser().parse(text.toString());
   }
-  expect(wrapper).toThrowError('Invalid command number 2 (starts at line 12)\ninvalid number of brackets');
+  expect(wrapper).toThrowError('Invalid command number 4 (starts at line 12)\ninvalid number of brackets');
 
+});
+
+test('test invalid number of braсes {}', () => {
+  function wrapper () {
+    const text = fs.readFileSync('examples/wrong/getVbar_brackets.js');
+    return new parser().parse(text.toString());
+  }
+  expect(wrapper).toThrowError('Invalid command number 0 (starts at line 9)\nUnexpected token } in JSON at position 13');
 });
